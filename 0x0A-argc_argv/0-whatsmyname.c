00f7a3e5e3315cef341fcd8cc103bdafe37663b7
fz-aitsiali <fz.aitsiali@gmail.com>
2023-04-10 17:15:51 -0700

    s

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the name of the program
+ * followed by a new line
+ * @argc : an argument counter for char
+ * @argv : an argument value for char
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
