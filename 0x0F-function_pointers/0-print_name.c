f4e7dd6063f6a4324544e1a68853b82114497187
fz-aitsiali <fz.aitsiali@gmail.com>
2023-04-19 16:13:02 -0700

    z

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - print the name
@@ -7,12 +6,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
-	{
+	if (name != NULL && f != NULL)
 		f(name);
-	}
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * array_iterator - iterates through array
  * @array: array to go through
  * @size: size of array
  * @action: action function to take
- * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-if (array == NULL || action == NULL)
-{
-return;
-}
-for (i = 0; i < size; i++)
-{
-action(array[i]);
-}
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		exit(98);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
